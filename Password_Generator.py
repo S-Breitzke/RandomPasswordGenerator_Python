0c88177255238b7f85d1d25994b41c4ad1770139
Simon Breitzke <s.breitzke@icloud.com>
2025-07-19 15:45:33 +0200

    first commit

---
+++ Password_Generator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+#determines length of your password
+length = int(input("Enter Length: "))
+
+#adds upper-,lowercase letters and digits
+chars = string.ascii_letters
+chars += string.digits
+
+password = ""
+print("Should your password include punctuation?")
+passwordStrength = int(input("Enter 1 or 2: "))
+if(passwordStrength == 1):
+    #adds punctuation to char string
+    chars += string.punctuation
+    #generates password 
+    for i in range(length):
+        next_character = random.choice(chars)
+        password += next_character
+elif(passwordStrength == 2):
+    #generates password without punctuation
+    for i in range(length):
+        next_character = random.choice(chars)
+        password += next_character
+else:
+    print("Not 1 or 2")
+
+print("Your random Password is:", password)
